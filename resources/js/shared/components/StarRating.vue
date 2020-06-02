1c98bb40b1a8c6fde56538ad06b46df5c1b6c5cf
AdrianDanlos <adrianf1team@gmail.com>
2020-06-02 16:27:54 +0200

    star-rating hover try

--- resources/js/shared/components/StarRating.vue
+++ resources/js/shared/components/StarRating.vue
@@ -5,14 +5,16 @@
       class="fas fa-star"
       v-for="star in fullStars"
       :key="'full' + star"
-      @click="$emit('input', star)"
+      @click="$emit('input', star); switchHover(star, 'full')"
+      @mouseover="$emit('input', hover ? star : starsSaved)"
     ></i>
     <i class="fas fa-star-half-alt" v-if="halfStar"></i>
     <i
       class="far fa-star"
       v-for="star in emptyStars"
       :key="'empty' + star"
-      @click="$emit('input', fullStars + star)"
+      @click="$emit('input', fullstars + star); switchHover(star, 'empty')"
+      @mouseover="$emit('input', hover ? fullStars + star : starsSaved)"
     ></i>
   </div>
 </template>
@@ -23,18 +25,45 @@ export default {
   props: {
     value: Number
   },
+  data() {
+    return {
+      hover: true,
+      starsSaved: 0
+    };
+  },
+  methods: {
+    switchHover(star, type) {
+      if (type === "empty") {
+        console.log("empty");
+        console.log("star: " + star);
+        this.value = star + (5 - star);
+        console.log(this.value);
+        console.log("--end-empty--");
+      } else {
+        this.value = star;
+      }
+      this.starsSaved = this.value;
+      console.log(this.hover);
+      this.hover = !this.hover;
+      console.log(this.hover);
+    }
+  },
   computed: {
     halfStar() {
       //logic: if rating(value) is 4.1 or above display 4.5
       return this.value - Math.floor(this.value) > 0.25 ? 1 : 0;
     },
     fullStars() {
+      console.log('fullstars')
+      console.log(this.value)
+      console.log(Math.floor(this.value))
       return Math.floor(this.value);
     },
     emptyStars() {
       //if we have added a halfstar add floor.emptyStarts. If we havent, add ceil.emptyStars
       return Math.floor(
-        5 - (this.halfStar === 0 ? Math.floor(this.value) : Math.ceil(this.value))
+        5 -
+          (this.halfStar === 0 ? Math.floor(this.value) : Math.ceil(this.value))
       );
     }
   }
